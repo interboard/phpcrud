933d2a189327ca1f0f6e5ac7b2cc3dcfa58d29a6
interboard <interboard@gmail.com>
2017-09-24 23:24:08 +0800

    test
    test

---
+++ sqlconfig.php
@@ -0,0 +1,26 @@
+<?php
+class sqlconfig
+{
+    private $_host = 'localhost';
+    private $_username = 'interboard';
+    private $_password = 'fts776sut';
+    private $_database = 'interboard';
+
+    protected $connection;
+
+    public function __construct()
+    {
+        if (!isset($this->connection)) {
+
+            $this->connection = new mysqli($this->_host, $this->_username, $this->_password, $this->_database);
+
+            if (!$this->connection) {
+                echo 'Cannot connect to database server';
+                exit;
+            }
+        }
+
+        return $this->connection;
+    }
+}
+?>
